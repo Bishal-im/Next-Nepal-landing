abb75016a206c47866d47981ad354d49b3d43532
Bishal Pandey <bishalpandey.im@gmail.com>
2026-02-12 12:32:43 +0545

    Fix tailwind.config.ts merge conflicts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,10 @@
 import type { Config } from "tailwindcss";
 
-<<<<<<< HEAD
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-=======
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> 1e06c944aa2f7d79d84316c853f4ac051f5e9c83
   ],
   theme: {
     extend: {
@@ -23,9 +15,6 @@ const config: Config = {
     },
   },
   plugins: [],
-<<<<<<< HEAD
-} satisfies Config;
-=======
 };
+
 export default config;
->>>>>>> 1e06c944aa2f7d79d84316c853f4ac051f5e9c83
